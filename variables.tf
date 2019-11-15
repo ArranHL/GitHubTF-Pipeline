20c979c2e01951ef66c01ab28d3c6a99071459cb
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-11-15 18:09:14 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,9 +2,11 @@ variable "notcount" {
   default = 1	
 }
 
+#Specify the organization you will be modifying here 
+#Replace the default value with the name of your organization
   variable "github_organization" {
   description = "Organization Name"
-  default     = "TerrifiedSquid"
+  default     = "Your_Org_Name"
 }
 
 # The following variable is a placeholder for the Vault crednetials provided. 
